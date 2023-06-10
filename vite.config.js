8ee2a39863b22f992da6ecaa84f803ce7ff97b03
mohamednegm169 <mohamed.khaled.negm1994@gmail.com>
2023-06-10 11:30:13 +0300

    add rules to vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [removeHistoryFallback(), react()],
+});
+
+const removeViteSpaFallbackMiddleware = (middlewares) => {
+  const { stack } = middlewares;
+  const index = stack.findIndex(
+    ({ handle }) => handle.name === "viteSpaFallbackMiddleware"
+  );
+  if (index > -1) {
+    stack.splice(index, 1);
+  } else {
+    throw Error("viteSpaFallbackMiddleware() not found in server middleware");
+  }
+};
+
+const removeHistoryFallback = () => {
+  return {
+    name: "remove-history-fallback",
+    apply: "serve",
+    enforce: "post",
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use("/", (req, _, next) => {
+        if (req.url === "/") {
+          req.url = "/index.html";
+        }
+        next();
+      });
+
+      return () => removeViteSpaFallbackMiddleware(server.middlewares);
+    },
+  };
+};

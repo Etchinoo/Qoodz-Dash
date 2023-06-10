9a38e6911d352002532a6ec7120f07c54c11ef26
mohamednegm169 <mohamed.khaled.negm1994@gmail.com>
2023-06-10 12:02:02 +0300

    change vite config v4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-import {resolve} from "path";
-import ViteRedirect404Plugin from "./vite.redirect";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ViteRedirect404Plugin(), react()],
-});
-
+  plugins: [react()]
+})

f37c6a5affbe353b87b0ded0170382a339c91915
mohamednegm169 <mohamed.khaled.negm1994@gmail.com>
2023-06-10 11:38:20 +0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [removeHistoryFallback(), react()],
-});
-
 const removeViteSpaFallbackMiddleware = (middlewares) => {
   const { stack } = middlewares;
   const index = stack.findIndex(
@@ -36,3 +31,9 @@ const removeHistoryFallback = () => {
     },
   };
 };
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [removeHistoryFallback(), react()],
+});
+

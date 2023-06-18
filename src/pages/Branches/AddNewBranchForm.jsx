6a51837f7ae09c05981c00c6c5b102c8be0caf47
mohamednegm169 <mohamed.khaled.negm1994@gmail.com>
2023-06-18 12:19:20 +0300

    Implement add new branch form

--- src/pages/Branches/AddNewBranchForm.jsx
+++ src/pages/Branches/AddNewBranchForm.jsx
@@ -1,24 +1,101 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 import styled from "styled-components";
 import "react-phone-number-input/style.css";
-import PhoneInput from "react-phone-number-input";
 import { Col, Row } from "../../components/Shared";
-import Select from "react-select";
-import PrimButton from "../../components/PrimButton";
-// import PhoneInput from "react-phone-number-input";
-import checkmark from "../../assets/checkmark.png";
-import { Form, Input, InputGrp, Label, Title } from "../Cashires/FormComponents.styles";
-import SuccessModal from "../../components/Shared/SuccessModal";
 
-export  default function AddNewBranchForm() {
+import {
+  Form,
+  Input,
+  InputGrp,
+  Label,
+  Title,
+} from "../Cashires/FormComponents.styles";
+import SuccessModal from "../../components/Shared/SuccessModal";
+import { PrimaryBtn, SSelect } from "../Cashires/FormComponents.styles";
+import axios from "axios";
+import { userAtom, userTokenAtom } from "../../store/Atoms";
+import { useAtom } from "jotai";
+import { APIsConstants } from "../../constants/API.constants";
+
+export default function AddNewBranchForm({ locations }) {
   const [stage, setStage] = useState(1);
+  const [name, setName] = useState(null);
+  const [phone, setPhone] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [area, setArea] = useState(null);
+  const [areas, setAreas] = useState([]);
+
+  const [token, setToken] = useAtom(userTokenAtom);
+  const [user, setUser] = useAtom(userAtom);
+
+  const CreateBranch = () => {
+    let data = {
+      phoneNumber: phone,
+      name: name,
+      location: location.value,
+      area: location.value,
+    };
+    axios
+      .post(`${APIsConstants.BASE_URL}/branches`, data, {
+        headers: {
+          "Content-Type": "application/json",
+          apiKey: "63cad87c3207fce093f8c08388e5a805",
+          Authorization: `Bearer ${token?.accessToken}`,
+        },
+      })
+      .then((res) => {
+        setStage(2);
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          setToken(null);
+          setUser(null);
+        } else {
+          setError(error.response.data.message);
+        }
+      });
+  };
+  const GetAreas = async () => {
+    axios
+      .get(
+        `${APIsConstants.BASE_URL}/locations/areas?location=${location.value}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            apiKey: "63cad87c3207fce093f8c08388e5a805",
+            Authorization: `Bearer ${token?.accessToken}`,
+          },
+        }
+      )
+      .then((res) =>
+        setAreas(
+          res.data.map((ele) => {
+            return { value: ele.id, label: ele.name };
+          })
+        )
+      )
+      .catch((error) => {
+        console.log("error: ", error.response.status);
+        if (error.response.status === 401) {
+          setToken(null);
+          setUser(null);
+        }
+      });
+  };
+  useEffect(() => {
+    if (location) {
+      GetAreas();
+    }
+  }, [location]);
+
   const onSubmit = () => {
-    console.log("Done");
-    setStage(2);
+    CreateBranch();
   };
 
   if (stage === 2)
-  return <SuccessModal mainText={"Branch Successfully Added!"} />
+    return <SuccessModal mainText={"Branch Successfully Added!"} />;
   if (stage === 1)
     return (
       <Form>
@@ -26,23 +103,61 @@ export  default function AddNewBranchForm() {
         <Row gap={"2rem"}>
           <InputGrp>
             <Label>Branch Name</Label>
-            <Input type="text" placeholder="John Doe" name="name" />
+            <Input
+              type="text"
+              placeholder="John Doe"
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
           </InputGrp>
           <InputGrp>
             <Label>Branch Phone Number</Label>
-            <Input type="text" placeholder="+2092089204" name="phone" />
+            <PhoneInput
+              containerClass=""
+              inputProps={{
+                name: null,
+                required: true,
+              }}
+              specialLabel={null}
+              inputClass="phoneInputWrapper"
+              country="eg"
+              onChange={(data) => setPhone(data)}
+              value={phone}
+            />
           </InputGrp>
         </Row>{" "}
-        <Row>
+        <InputGrp>
+          <Label>Branch Location</Label>
+          <SSelect
+            className="select-filter"
+            classNamePrefix="filter-opt"
+            isClearable={true}
+            isSearchable={true}
+            placeholder="Select Location"
+            options={locations}
+            onChange={(e) => setLocation(e)}
+          />
+        </InputGrp>
+        {location && (
           <InputGrp>
-            <Label>Branch Address</Label>
-            <Input type="text" placeholder="" name="address" />
+            <Label>Branch Area</Label>
+            <SSelect
+              className="select-filter"
+              classNamePrefix="filter-opt"
+              isClearable={true}
+              isSearchable={true}
+              placeholder="Select Area"
+              options={areas}
+              onChange={(e) => setArea(e)}
+            />
           </InputGrp>
-        </Row>
-        <PrimButton onClick={() => onSubmit()}>Request Branch</PrimButton>
+        )}
+        <PrimaryBtn
+          disabled={!(name && phone && location && area)}
+          onClick={() => onSubmit()}
+        >
+          Request Branch
+        </PrimaryBtn>
       </Form>
     );
 }
-
-
-
